b4ea6f258c0844f611549c0fbabc219592623058
Zakaria El Berkani <zakaria.elberkani@barmer.de>
2023-11-07 19:35:08 +0100

    Alle Chareckter der Api auflisten

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
-import "./styles.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import CardGrid from "./components/CardGrid";
 import { Stack } from "@mui/material";
 
-export default function App() {
-  const url = "https://rickandmortyapi.com/api/character/";
-  const [data, setData] = useState([]);
+interface Character {
+  id: number;
+  name: string;
+  // Weitere Eigenschaften hier
+}
 
-  const fetchData = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d.results))
+interface CharacterResponse {
+  info: {
+    count: number;
+    pages: number;
+  };
+  results: Character[];
+}
+
+export default function App() {
+  const baseUrl = "https://rickandmortyapi.com/api/character/";
+  const [data, setData] = useState<Character[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchData = (page: number) => {
+    fetch(`${baseUrl}?page=${page}`)
+      .then((res) => res.json() as Promise<CharacterResponse>)
+      .then((d) => {
+        setData((prevData) => [...prevData, ...d.results]);
+        setTotalPages(d.info.pages);
+      })
       .catch((error) => console.error("Error fetching data: ", error));
   };
 
   useEffect(() => {
-    fetchData();
-    console.log(data);
-  }, []);
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  }, [totalPages, currentPage]);
 
   return (
-    <div className="App" style={{width:"100vw"}}>
-      <Stack justifyContent="center" alignItems='center'>
-      <Filter />
-      <CardGrid data = {data}/>
+    <div className="App" style={{ width: "100vw" }}>
+      <Stack justifyContent="center" alignItems="center">
+        <Filter />
+        <CardGrid data={data} />
       </Stack>
     </div>
   );
