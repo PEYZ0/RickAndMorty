4d25db0aae1c7c1b7014ea902068c0094cb3b24e
PEYZ0 <zakaria.elberkani@barmer.de>
2023-11-07 22:28:34 +0100

    Doppelte einträge bug gefixed

--- src/App.tsx
+++ src/App.tsx
@@ -21,27 +21,33 @@ export default function App() {
   const baseUrl = "https://rickandmortyapi.com/api/character/";
   const [data, setData] = useState<Character[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
+  const [initialFetchComplete, setInitialFetchComplete] = useState(false);
 
   const fetchData = (page: number) => {
     fetch(`${baseUrl}?page=${page}`)
       .then((res) => res.json() as Promise<CharacterResponse>)
       .then((d) => {
-        setData((prevData) => [...prevData, ...d.results]);
+        if (!initialFetchComplete) {
+          setInitialFetchComplete(true);
+          return;
+        }
+        // Überprüfen, ob die Daten bereits in "data" vorhanden sind
+        const newData = d.results.filter((result) => {
+          return !data.some((existingData) => existingData.id === result.id);
+        });
+        setData((prevData) => [...prevData, ...newData]);
         setTotalPages(d.info.pages);
+        if (currentPage < d.info.pages) {
+          setCurrentPage(currentPage + 1);
+        }
       })
       .catch((error) => console.error("Error fetching data: ", error));
   };
 
   useEffect(() => {
     fetchData(currentPage);
-  }, [currentPage]);
-
-  useEffect(() => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  }, [totalPages, currentPage]);
+  }, [currentPage, data, initialFetchComplete]);
 
   return (
     <div className="App" style={{ width: "100vw" }}>
