16c30cde2b7f09ac886a50884e2f9ba7a4dd5461
PEYZ0 <zakaria.elberkani@barmer.de>
2023-11-27 12:21:14 +0100

    Crash wenn keine Daten da sind Fix

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -15,31 +15,46 @@ function CardGrid(props: any) {
     }
    
   }
+  const hasData = (obj:object|undefined|null)=>{
+    if (obj!= null ) {
+      return Object.keys(obj).length > 0;
+    }else{
+      return false;
+    }
+  }
   return (
     <>
+    
       <ThemeProvider theme={darkTheme}>
-      <Stack m={2}>
-          <Pagination count={props.totalPages}  page={props.currentPage} onChange={handleChange} />
-        </Stack>
-        <CssBaseline />
-        {<Stack
-          direction="row"
-          flexWrap="wrap"
-          alignContent="center"
-          justifyContent="center"
-          width="80%"
-        >
-          {props.data.map((character: any) => (
-            <Card
-              key={character.id}
-              image={character.image}
-              name={character.name}
-              gender={character.gender}
-              status={character.status}
-              species={character.species}
-            />
-          ))}
-        </Stack>}
+      {hasData(props.data)?
+      <>
+        <Stack m={2}>
+        <Pagination count={props.totalPages}  page={props.currentPage} onChange={handleChange} />
+      </Stack>
+      <CssBaseline />
+      {<Stack
+        direction="row"
+        flexWrap="wrap"
+        alignContent="center"
+        justifyContent="center"
+        width="80%"
+      >
+        {props.data.map((character: any) => (
+          <Card
+            key={character.id}
+            image={character.image}
+            name={character.name}
+            gender={character.gender}
+            status={character.status}
+            species={character.species}
+          />
+        ))}
+      </Stack>}
+      </> :
+      <>
+        <h2> Es wurden keine Charackter gefunden</h2>
+      </>
+      }
       </ThemeProvider>
     </>
   );
